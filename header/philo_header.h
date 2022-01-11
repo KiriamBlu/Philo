03689e6e09a9f5e17425fb7c35910f3704c5d060
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2022-01-11 15:56:44 +0100

    times done

--- header/philo_header.h
+++ header/philo_header.h
@@ -6,7 +6,7 @@
 /*   By: jsanfeli <jsanfeli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 12:30:15 by jsanfeli          #+#    #+#             */
-/*   Updated: 2022/01/10 17:29:35 by jsanfeli         ###   ########.fr       */
+/*   Updated: 2022/01/11 15:45:00 by jsanfeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,22 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <limits.h>
+#include <sys/time.h>
 
 typedef struct t_mutex
 {
-	pthread_t *thread;
-	pthread_mutex_t mutex;
 	int philo_num;
-	int i;
+	pthread_mutex_t	*forks;
+	struct timeval reftime;
+	unsigned long time;
 }gen;
 
 typedef struct t_philo
 {
+	pthread_t thread;
+	pthread_mutex_t	*left;
+	pthread_mutex_t	*right;
+	unsigned long lasttime;
 	int index;
 	gen lst;
 }philo;
--- sources/philo.c
+++ sources/philo.c
@@ -6,7 +6,7 @@
 /*   By: jsanfeli <jsanfeli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 14:50:35 by jsanfeli          #+#    #+#             */
-/*   Updated: 2022/01/10 17:29:31 by jsanfeli         ###   ########.fr       */
+/*   Updated: 2022/01/11 15:50:28 by jsanfeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,59 @@
 #include <pthread.h>
 # include"../header/philo_header.h"
 
-void ft_putstr(char *str)
+void getupdatetime(philo  *ph)
 {
-	int i;
+	unsigned long i;
 
-	i = 0;
-	while (str[i])
-	{
-		putchar(str[i]);
-		i++;
-	}
+	gettimeofday(&ph->lst.reftime, NULL);
+	i = ((unsigned long)ph->lst.reftime.tv_sec * 1000) + ((unsigned long)ph->lst.reftime.tv_usec / 1000);
+	ph->lasttime = i - ph->lst.time;
 }
 
 void *managment(void *prueba)
 {
 	philo *ph;
-
 	ph = (philo *)prueba;
-	pthread_mutex_lock(&ph->lst.mutex);
-	ph->lst.i++;
-	printf("%i = %i\n", ph->index, ph->lst.i);
-	pthread_mutex_unlock(&ph->lst.mutex);
+	if (ph->index % 2 == 0)
+			usleep(100);
+	while(1)
+	{
+		getupdatetime(ph);
+		printf("Timecheck:%lu\n", ph->lasttime);
+		pthread_mutex_lock(ph->left);
+		printf("Philo:%d left pick\n", ph->index);
+		pthread_mutex_lock(ph->right);
+		printf("Philo:%d right pick\n", ph->index);
+		pthread_mutex_unlock(ph->left);
+		pthread_mutex_unlock(ph->right);
+		usleep(1000);
+	}
 	return (0);
+}
+
+void phinit(philo *ph, gen *philo_gen)
+{
+	int i;
+
+	i = 0;
+	while(i < philo_gen->philo_num - 1)
+	{
+		pthread_mutex_init(&philo_gen->forks[i], NULL);
+		pthread_mutex_init(&philo_gen->forks[i + 1], NULL);
+		ph[i].left = &philo_gen->forks[i];
+		ph[i].right = &philo_gen->forks[i + 1];
+		ph[i].lst = *philo_gen;
+		ph[i].index = i;
+		ph[i].thread = malloc(sizeof(pthread_t) * 1);
+		ph[i].lasttime = 0;
+		i++;
+	}
+	pthread_mutex_init(&philo_gen->forks[i], NULL);
+	ph[i].left = &philo_gen->forks[i];
+	ph[i].right = &philo_gen->forks[0];
+	ph[i].lst = *philo_gen;
+	ph[i].index = i;
+	ph[i].thread = malloc(sizeof(pthread_t) * 1);
 }
 
 int main(int argc, char const *argv[])
@@ -46,24 +77,22 @@ int main(int argc, char const *argv[])
 	philo  *ph;
 	int i;
 
-	i = 0;
-	if(argc < 2)
+	if(argc < 2 | gettimeofday(&philo_gen.reftime, NULL) == -1)
 	{
 		printf("ERROR\n");
 		return(0);
 	}
-	philo_gen.i = 0;
+	philo_gen.time = ((unsigned long)philo_gen.reftime.tv_sec * 1000) + ((unsigned long)philo_gen.reftime.tv_usec / 1000);
+	printf("%lu\n", philo_gen.time);
 	philo_gen.philo_num = ft_atoi_special(argv[1]);
-	philo_gen.thread = malloc(sizeof(pthread_t) * philo_gen.philo_num);
+	philo_gen.forks = malloc(sizeof(pthread_mutex_t) * philo_gen.philo_num);
 	ph = malloc(sizeof(philo) * philo_gen.philo_num);
-	pthread_mutex_init(&philo_gen.mutex, NULL);
-	while(i < philo_gen.philo_num)
-	{
-		ph[i].lst = philo_gen;
-		ph[i].index = i;
-		pthread_create(&philo_gen.thread[i], NULL, managment, &ph[i]);
-		i++;
-	}
+	phinit(ph, &philo_gen);
+	i = -1;
+	while(++i < philo_gen.philo_num)
+		pthread_create(&ph[i].thread, NULL, managment, &ph[i]);
+	while(1)
+		;
 	usleep(100000);
 	return 0;
 }
